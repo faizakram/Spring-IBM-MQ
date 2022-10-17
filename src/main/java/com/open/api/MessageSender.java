a23a0025bc61ce0d85b5da2173329ad641afc01a
Faiz Akram <faiz.krm@gmail.com>
2022-10-17 23:29:49 +0530

    Update MessageSender.java

--- src/main/java/com/open/api/MessageSender.java
+++ src/main/java/com/open/api/MessageSender.java
@@ -2,6 +2,7 @@ package com.open.api;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
+import javax.jms.MessageConsumer;
 import javax.jms.Session;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,31 @@ public class MessageSender {
 				return session.createTextMessage("ABC");
 			}
 		});
+	}
+
+	public String sendMessageAndReceive() {
+		this.jmsTemplate.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+		this.jmsTemplate.convertAndSend(destination, "bar");
+		try {
+			String value = this.jmsTemplate.execute(session -> {
+				MessageConsumer consumer = session.createConsumer(
+						this.jmsTemplate.getDestinationResolver().resolveDestinationName(session, destination, false));
+				String result;
+				try {
+					Message received = consumer.receive(5000);
+					result = (String) this.jmsTemplate.getMessageConverter().fromMessage(received);
+					// Do some stuff that might throw an exception
+					received.acknowledge();
+				} finally {
+					consumer.close();
+				}
+				return result;
+			}, true);
+			return value;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
